eb42cfb9fd431b606b8017f1e37d5d3cbe52a0c2
dongxincong <1414423031@qq.com>
2018-08-01 17:05:47 +0800

    ..

---
+++ letme-demo/js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+	// 设置边框线的高度 = 屏幕可视区域高度 - 头部的宽度
+	var screenHeight = $(window).height() - 41;
+	$(".effect").height(screenHeight);
+    
+	$(".btn").click(function() {
+		var data = $(".textarea").val();
+		$(".example").html(data);
+	})
+
+	$(".btn-js").click(function() {
+		var data = $(".js").val();
+		eval(data);
+	})
+})
